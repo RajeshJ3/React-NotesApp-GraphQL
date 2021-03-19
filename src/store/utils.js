688e2fee0fb9aab1ee188d3449ddc6e1323c6e21
Rajesh Joshi <joshirajesh448@gmail.com>
2021-03-19 12:18:08 +0530

    edited domains

--- src/store/utils.js
+++ src/store/utils.js
@@ -1,8 +1,10 @@
 function get_domain() {
   if (window.location.hostname === "localhost") {
     return "http://0.0.0.0:8000";
-  } else {
+  } else if (window.location.hostname === "192.168.43.207") {
     return "http://192.168.43.207:8000";
+  } else {
+    return "https://rajeshj3.pythonanywhere.com";
   }
 }
 
